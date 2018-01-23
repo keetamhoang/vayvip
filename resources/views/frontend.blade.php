e4e9b8c189217e8541de226f33ff521a805b9219
Kee Tam Hoàng <Kee Tam Hong>
2018-01-23 23:27:27 +0700

    update

--- resources/views/frontend.blade.php
+++ resources/views/frontend.blade.php
@@ -207,6 +207,28 @@
         </div>
     </div>
 </div>
+
+<div style="margin-top: 390px">
+    <a  href=".dang-ky-modal" data-toggle="modal">Click</a>
+</div>
+
+<div class="modal fade dang-ky-modal" style="z-index: 1071;margin-top: 100px;" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered " role="document">
+        <div class="modal-content">
+            <div class="modal-header" style="padding-bottom: 5px;">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+
+            </div>
+            <div class="modal-footer">
+
+            </div>
+        </div>
+    </div>
+</div>
 <!--end header  area -->
 
 @yield('content')
