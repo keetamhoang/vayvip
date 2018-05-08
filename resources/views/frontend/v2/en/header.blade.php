4e3b0ccf897a6abd732c120d40ac0d60fe367804
Kee Tam Hoàng <Kee Tam Hong>
2018-05-09 00:10:21 +0700

    updaty

--- resources/views/frontend/v2/en/header.blade.php
+++ resources/views/frontend/v2/en/header.blade.php
@@ -17,16 +17,13 @@
                 <div class="menu-extras col-lg-9 col-xs-5 col-sm-9">
                     <ul class="nav navbar-nav navbar-right pull-right">
                         <li class="hidden-xs">
-                            <a href="{{ url('ma-giam-gia') }}">MÃ GIẢM GIÁ MỚI</a>
+                            <a href="{{ url('coupons') }}"><i class="ti-gift"></i> COUPONS %</a>
                         </li>
                         <li class="hidden-xs">
-                            <a href="{{ url('vay-von-tin-dung') }}">VAY VỐN TÍN DỤNG</a>
+                            <a href="{{ url('products') }}"><i class="ti-bag"></i> PRODUCTS</a>
                         </li>
                         <li class="hidden-xs">
-                            <a href="{{ url('tin-tuc-tai-chinh') }}">TIN TỨC TÀI CHÍNH</a>
-                        </li>
-                        <li class="hidden-xs">
-                            <a href="{{ url('mua-sam-hom-nay') }}">MUA SẮM HÔM NAY</a>
+                            <a href="{{ url('trends') }}"><i class="ti-stats-up"></i> TRENDS #</a>
                         </li>
                         @if (auth('admin')->check())
                             <li class="hidden-xs">
@@ -50,44 +47,50 @@
             <div id="navigation">
                 <!-- Navigation Menu-->
                 <ul class="navigation-menu">
-                    <li class="active"> <a href="{{ url('ma-giam-gia/ma-giam-gia-hot') }}"><i class="ti-crown"></i> <span> Mã giảm giá HOT </span> </a> </li>
-                    <li class="has-submenu">
-                        <a href="#"><i class="ti-gift"></i> <span> Mã giảm giá theo đơn vị </span> </a>
-                        <ul class="submenu">
-                            <li><a href="{{ url('ma-giam-gia/ma-giam-gia-lazada') }}">Mã giảm giá Lazada</a> </li>
-                            <li><a href="{{ url('ma-giam-gia/ma-giam-gia-tiki') }}">Mã giảm giá Tiki</a> </li>
-                            <li><a href="{{ url('ma-giam-gia/ma-giam-gia-shopee') }}">Mã giảm giá Shopee</a> </li>
-                            <li><a href="{{ url('ma-giam-gia/ma-giam-gia-grab') }}">Mã giảm giá Grab</a> </li>
-                            <li><a href="{{ url('ma-giam-gia/ma-giam-gia-yes24') }}">Mã giảm giá Yes24</a> </li>
-                            <li><a href="{{ url('ma-giam-gia/ma-giam-gia-adayroi') }}">Mã giảm giá Adayroi</a> </li>
-                            <li><a href="{{ url('ma-giam-gia/ma-giam-gia-du-lich') }}">Mã giảm giá du lịch</a> </li>
-                            <li><a href="{{ url('ma-giam-gia/ma-giam-gia-lotte') }}">Mã giảm giá Lotte</a> </li>
-                        </ul>
-                    </li>
-                    <li class="has-submenu">
-                        <a href="#"><i class="ti-tablet"></i> <span> Sản phẩm giảm giá </span> </a>
-                        <ul class="submenu">
-                            <li><a href="{{ url('ma-giam-gia/ma-giam-gia-san-pham-dien-tu-cong-nghe') }}">Giảm giá điện tử - công nghệ</a> </li>
-                            <li><a href="{{ url('ma-giam-gia/do-gia-dung-giam-gia') }}">Đồ gia dụng giảm giá</a> </li>
-                            <li><a href="{{ url('ma-giam-gia/ma-giam-gia-cho-me-va-be') }}">Giảm giá cho mẹ và bé</a> </li>
-                            <li><a href="{{ url('ma-giam-gia/ma-giam-gia-lam-dep') }}">Giảm giá dịch vụ làm đẹp</a> </li>
-                            <li><a href="{{ url('ma-giam-gia/ma-giam-gia-du-lich-2') }}">Giảm giá du lịch</a> </li>
-                            <li><a href="{{ url('ma-giam-gia/ma-giam-gia-thoi-trang') }}">Giảm giá thời trang</a> </li>
-                            <li><a href="{{ url('ma-giam-gia/ma-giam-gia-nha-cua-doi-song') }}">Giảm giá nhà cửa đời sống</a> </li>
-                            <li><a href="{{ url('ma-giam-gia/dich-vu-giam-gia') }}">Dịch vụ giảm giá</a> </li>
-                            <li><a href="{{ url('ma-giam-gia/ma-giam-gia-bach-hoa') }}">Giảm giá bách hóa</a> </li>
-                            <li><a href="{{ url('ma-giam-gia/sach-giam-gia') }}">Sách giảm giá</a> </li>
-                            <li><a href="{{ url('ma-giam-gia/xe-may-giam-gia') }}">Xe máy giảm giá</a> </li>
-                            <li><a href="{{ url('ma-giam-gia/ma-giam-gia-ngan-hang') }}">Giảm giá qua ngân hàng</a> </li>
-                        </ul>
-                    </li>
-                    <li class="has-submenu">
-                        <a href="#"><i class="ti-comments-smiley"></i> <span> Nhận thông tin mã giảm giá </span> </a>
-                        <ul class="submenu">
-                            <li><a href="{{ url('ma-giam-gia/nhan-ma-giam-gia-qua-inbox') }}">Nhận mã giảm giá qua Inbox</a> </li>
-                            <li><a href="{{ url('ma-giam-gia/nhan-ma-giam-gia-qua-email') }}">Nhận mã giảm giá qua Email</a> </li>
-                        </ul>
-                    </li>
+                    <li class=""> <a href="{{ url('ma-giam-gia/ma-giam-gia-hot') }}" ><i class="i-electronics common-global-icons-sprite"></i> <span>  Electronics  </span> </a> </li>
+                    <li class=""> <a href="{{ url('ma-giam-gia/ma-giam-gia-hot') }}"><i class="i-fashion common-global-icons-sprite"></i> <span>   Fashion   </span> </a> </li>
+                    <li class=""> <a href="{{ url('ma-giam-gia/ma-giam-gia-hot') }}"><i class="i-sports-outdoor common-global-icons-sprite"></i> <span>  Sports & Outdoors </span> </a> </li>
+                    <li class=""> <a href="{{ url('ma-giam-gia/ma-giam-gia-hot') }}"><i class="i-home-living common-global-icons-sprite"></i> <span>  Home & Living</span> </a> </li>
+                    <li class=""> <a href="{{ url('ma-giam-gia/ma-giam-gia-hot') }}"><i class="i-kids-toys common-global-icons-sprite"></i> <span>  Kids & Toys </span> </a> </li>
+                    <li class=""> <a href="{{ url('ma-giam-gia/ma-giam-gia-hot') }}"><i class="i-health-beauty common-global-icons-sprite"></i> <span>  Automotive </span> </a> </li>
+                    <li class=""> <a href="{{ url('ma-giam-gia/ma-giam-gia-hot') }}"><i class="i-others common-global-icons-sprite icon"></i> <span>  See More </span> </a> </li>
+                    {{--<li class="has-submenu">--}}
+                        {{--<a href="#"><i class="ti-gift"></i> <span> Mã giảm giá theo đơn vị </span> </a>--}}
+                        {{--<ul class="submenu">--}}
+                            {{--<li><a href="{{ url('ma-giam-gia/ma-giam-gia-lazada') }}">Mã giảm giá Lazada</a> </li>--}}
+                            {{--<li><a href="{{ url('ma-giam-gia/ma-giam-gia-tiki') }}">Mã giảm giá Tiki</a> </li>--}}
+                            {{--<li><a href="{{ url('ma-giam-gia/ma-giam-gia-shopee') }}">Mã giảm giá Shopee</a> </li>--}}
+                            {{--<li><a href="{{ url('ma-giam-gia/ma-giam-gia-grab') }}">Mã giảm giá Grab</a> </li>--}}
+                            {{--<li><a href="{{ url('ma-giam-gia/ma-giam-gia-yes24') }}">Mã giảm giá Yes24</a> </li>--}}
+                            {{--<li><a href="{{ url('ma-giam-gia/ma-giam-gia-adayroi') }}">Mã giảm giá Adayroi</a> </li>--}}
+                            {{--<li><a href="{{ url('ma-giam-gia/ma-giam-gia-du-lich') }}">Mã giảm giá du lịch</a> </li>--}}
+                            {{--<li><a href="{{ url('ma-giam-gia/ma-giam-gia-lotte') }}">Mã giảm giá Lotte</a> </li>--}}
+                        {{--</ul>--}}
+                    {{--</li>--}}
+                    {{--<li class="has-submenu">--}}
+                        {{--<a href="#"><i class="ti-tablet"></i> <span> Sản phẩm giảm giá </span> </a>--}}
+                        {{--<ul class="submenu">--}}
+                            {{--<li><a href="{{ url('ma-giam-gia/ma-giam-gia-san-pham-dien-tu-cong-nghe') }}">Giảm giá điện tử - công nghệ</a> </li>--}}
+                            {{--<li><a href="{{ url('ma-giam-gia/do-gia-dung-giam-gia') }}">Đồ gia dụng giảm giá</a> </li>--}}
+                            {{--<li><a href="{{ url('ma-giam-gia/ma-giam-gia-cho-me-va-be') }}">Giảm giá cho mẹ và bé</a> </li>--}}
+                            {{--<li><a href="{{ url('ma-giam-gia/ma-giam-gia-lam-dep') }}">Giảm giá dịch vụ làm đẹp</a> </li>--}}
+                            {{--<li><a href="{{ url('ma-giam-gia/ma-giam-gia-du-lich-2') }}">Giảm giá du lịch</a> </li>--}}
+                            {{--<li><a href="{{ url('ma-giam-gia/ma-giam-gia-thoi-trang') }}">Giảm giá thời trang</a> </li>--}}
+                            {{--<li><a href="{{ url('ma-giam-gia/ma-giam-gia-nha-cua-doi-song') }}">Giảm giá nhà cửa đời sống</a> </li>--}}
+                            {{--<li><a href="{{ url('ma-giam-gia/dich-vu-giam-gia') }}">Dịch vụ giảm giá</a> </li>--}}
+                            {{--<li><a href="{{ url('ma-giam-gia/ma-giam-gia-bach-hoa') }}">Giảm giá bách hóa</a> </li>--}}
+                            {{--<li><a href="{{ url('ma-giam-gia/sach-giam-gia') }}">Sách giảm giá</a> </li>--}}
+                            {{--<li><a href="{{ url('ma-giam-gia/xe-may-giam-gia') }}">Xe máy giảm giá</a> </li>--}}
+                            {{--<li><a href="{{ url('ma-giam-gia/ma-giam-gia-ngan-hang') }}">Giảm giá qua ngân hàng</a> </li>--}}
+                        {{--</ul>--}}
+                    {{--</li>--}}
+                    {{--<li class="has-submenu">--}}
+                        {{--<a href="#"><i class="ti-comments-smiley"></i> <span> Nhận thông tin mã giảm giá </span> </a>--}}
+                        {{--<ul class="submenu">--}}
+                            {{--<li><a href="{{ url('ma-giam-gia/nhan-ma-giam-gia-qua-inbox') }}">Nhận mã giảm giá qua Inbox</a> </li>--}}
+                            {{--<li><a href="{{ url('ma-giam-gia/nhan-ma-giam-gia-qua-email') }}">Nhận mã giảm giá qua Email</a> </li>--}}
+                        {{--</ul>--}}
+                    {{--</li>--}}
                 </ul>
                 <!-- End navigation menu  -->
             </div>
